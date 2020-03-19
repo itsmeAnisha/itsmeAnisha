bcebbd4c9dc7354f37138e85b9e1cdd3f7bc6add
itsmeAnisha <59765925+itsmeAnisha@users.noreply.github.com>
2020-03-18 22:23:26 -0700

    Add files via upload

---
+++ speakHello.js
@@ -0,0 +1,15 @@
+var speakWord = "Hello";
+
+// STEP 4: Rewrite the 'speak' function such that it is attached to the
+// helloSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+
+// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+// 'helloSpeaker' on the global scope as well.
+// See Lecture 52, part 2
+// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+// xxxx.xxxx = helloSpeaker;
+© 2020 GitHub, Inc.
